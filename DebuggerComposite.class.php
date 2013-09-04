9e06eafdad8678c2572a856ed4401f36b2c5db4b
maxorxy <home@diskussionsblog.de>
2013-09-04 10:04:07 +0200

    added comment in debuggercomposite.class.php

--- DebuggerComposite.class.php
+++ DebuggerComposite.class.php
@@ -2,7 +2,9 @@
 
 	class DebuggerComposite implements Debugger {
 		
+		/* Used Debuggers, Stored In An Array */
 		protected $debuggers = array();
+		
 		private $_messageIndicator = '';
 		private $_messageClassName = 'debug';
 		
